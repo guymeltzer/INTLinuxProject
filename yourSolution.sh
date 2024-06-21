fa751a637ee3c14ccde055d8cd6fa23ee2748323
Guy Meltzer <guymeltzer28@gmail.com>
2024-06-21 16:13:21 +0300

    answered questions 1-4 and added my fixed script to run the secret generator. For some reason - did not eppear in GitHub Actions - ran git push again
    put in the secret key in the first column.

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,5 +1,3 @@
-#wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
-#tar -xvf secretGenerator.tar.gz
 chmod +x generateSecret.sh
 mkdir secretDir
 rm -rf maliciousFiles
