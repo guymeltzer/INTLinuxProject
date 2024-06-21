eaa60a77a8b82e69298ff755b1811efdaf46f7fa
Guy Meltzer <guymeltzer28@gmail.com>
2024-06-21 13:19:38 +0300

    answered questions 1-4 and added my fixed script to run the secret generator.

---
+++ yourSolution.sh
@@ -0,0 +1,12 @@
+wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
+tar -xvf secretGenerator.tar.gz
+cd src
+chmod +x generateSecret.sh
+mkdir secretDir
+rm -rf maliciousFiles
+cd secretDir
+touch .secret
+chmod go-rw .secret
+cd ..
+rm important.link
+./generateSecret.sh
