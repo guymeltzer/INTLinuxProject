bf29cabfb998c5fa3e1a1d274d3d584326e0b023
Guy Meltzer <guymeltzer28@gmail.com>
2024-08-07 20:44:45 +0300

    Revert "answered questions 1-4 and added my fixed script to run the secret generator."
    This reverts commit fa751a637ee3c14ccde055d8cd6fa23ee2748323.

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,3 +1,5 @@
+#wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
+#tar -xvf secretGenerator.tar.gz
 chmod +x generateSecret.sh
 mkdir secretDir
 rm -rf maliciousFiles
